873e9287a4e52e9f3ed3d6aea49afd41875594c9
Steve Ham <steve.ham@kakaomobility.com>
2019-06-20 11:26:00 +0900

    Initial Commit

---
+++ CardSampleCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardSampleCode
+//
+//  Created by steve on 20/06/2019.
+//  Copyright © 2019 BrainTools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
